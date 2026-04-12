f7c208dca79feab80d772f2bc6e73d05ae4b948b
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-12 18:56:14 +0100

    feat: add Teachers Dashboard and Layout components

---
+++ app/(dashboard)/dashboard/teachers/page.jsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-3xl font-bold">Teachers Dashboard</h1>
+    </div>
+  );
+}
