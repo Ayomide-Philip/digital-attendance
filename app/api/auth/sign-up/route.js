127f3f3e1195b858cc9eea5c3d14d621a0645316
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-16 12:11:20 +0100

    feat: enhance sign-up API with comprehensive validation and add password hashing utility

--- app/api/auth/sign-up/route.js
+++ app/api/auth/sign-up/route.js
@@ -1,12 +1,24 @@
 import { NextResponse } from "next/server";
+import { connectDatabase } from "@/lib/database/connectdb";
+import User from "@/lib/models/user.model";
 
 export async function POST(req) {
   const { name, email, password } = await req.json();
   // checking if all fields are provided
-  if (!name || !name.trim()) {
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  if (
+    !name ||
+    !name.trim() ||
+    !email ||
+    !email.trim() ||
+    !password ||
+    !password.trim()
+  ) {
+    return NextResponse.json(
+      { error: "All fields are required" },
+      { status: 400 },
+    );
   }
-
+  // validating that the name has at least 2 parts (first and last name) and each part is at least 2 characters long
   if (!name.split(" ").every((part) => part.length >= 2)) {
     return NextResponse.json(
       { error: "Input your full name" },
@@ -25,5 +37,41 @@ export async function POST(req) {
     );
   }
 
-  return NextResponse.json({ name, email, password }, { status: 200 });
+  if (!/\S+@\S+\.\S+/.test(email.trim())) {
+    return NextResponse.json(
+      { error: "Invalid email format" },
+      { status: 400 },
+    );
+  }
+
+  if (password.trim().length < 8) {
+    return NextResponse.json(
+      { error: "Password must be at least 8 characters long" },
+      { status: 400 },
+    );
+  }
+
+  try {
+    await connectDatabase();
+    const userExists = await User.findOne({ email: email.trim() });
+
+    if(userExists){
+        return NextResponse.json({
+            error: "An account with this email already exists"
+        },{
+            status:401
+        })
+    }
+    return NextResponse.json({ name, email, password }, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      {
+        error: "An error occurred while creating your account",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
 }
