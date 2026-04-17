226dd19af8be79c9ba594c1991ee450817d4e1ef
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-17 20:44:46 +0100

    feat: enhance validation for class creation with improved name and code constraints

--- app/api/classes/route.js
+++ app/api/classes/route.js
@@ -4,6 +4,51 @@ export async function POST(req) {
   const { name, code, description, teacherId, emailSuffix, departmentalCode } =
     await req.json();
 
+  // logic for validating the input
+  if (!name || !name.trim()) {
+    return NextResponse.json(
+      {
+        error: "Name is required and cannot be empty",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  if (name.trim().length < 5 || name.trim().length > 50) {
+    return NextResponse.json(
+      {
+        error: "Name must be between 5 and 50 characters",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  if (!code || !code.trim()) {
+    return NextResponse.json(
+      {
+        error: "Code is required and cannot be empty",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  if (code.trim().length < 3 || code.trim().length > 50) {
+    return NextResponse.json(
+      {
+        error: "Code must be between 3 and 50 characters",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
   return NextResponse.json(
     {
       message: "Create a new class",
