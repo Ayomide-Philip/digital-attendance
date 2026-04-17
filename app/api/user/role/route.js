74dd16ca7cb5303d006553c10406e5493f8e4de5
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-17 12:30:37 +0100

    feat: implement user role update functionality with validation and error handling

--- app/api/user/role/route.js
+++ app/api/user/role/route.js
@@ -1,10 +1,77 @@
-export default async function PUT(req) {
+import { connectDatabase } from "@/lib/database/connectdb";
+import User from "@/lib/models/user.model";
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+export async function PUT(req) {
   const { role, userId } = await req.json();
 
-  return NextResponse.json(
-    { message: `User role updated to ${role}` },
-    {
-      status: 200,
-    },
-  );
+  if (!userId) {
+    return NextResponse.json(
+      {
+        error: "Invalid Parameters",
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+
+  if (!["student", "teacher"].includes(role.trim())) {
+    return NextResponse.json(
+      {
+        error: "Invalid role",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  try {
+    await connectDatabase();
+    // checking if user exist
+    const user = await User.findById(userId);
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "User does not exist",
+        },
+        {
+          status: 401,
+        },
+      );
+    }
+    // check if user role is not NA
+    if (user.role !== "NA") {
+      return NextResponse.json(
+        {
+          error: "User role cannot be updated",
+        },
+        {
+          status: 400,
+        },
+      );
+    }
+    await User.findByIdAndUpdate(user?._id, {
+      role: role.trim(),
+    });
+    return NextResponse.json(
+      {
+        message: `User role updated to ${role}`,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      {
+        error: "An error occurred while updating user role",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
 }
