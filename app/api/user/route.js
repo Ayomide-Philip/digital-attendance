1ab6d8f07334cce617b6e1b6d53ce746179d352e
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-19 10:46:03 +0100

    feat: enhance PUT method with user data validation and error handling

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -10,8 +10,75 @@ export async function GET(req) {
 }
 
 export const PUT = async function PUT(req) {
-  const { userId } = await req.json();
-  return NextResponse.json({
-    message: "User API is working with PUT method!",
-  });
+  const { userId, displayName, matricNo, department, level, school } =
+    await req.json();
+  // check if userId is provided and is not empty
+  if (!userId || !userId.trim()) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized Access",
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+  // validate displayName
+  if (displayName && displayName.trim().length < 5) {
+    return NextResponse.json(
+      {
+        error: "Display name must be at least 5 characters long",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+  // validate matricNo
+  if (matricNo && matricNo.trim().length < 5) {
+    return NextResponse.json(
+      {
+        error: "Matric number must be at least 5 characters long",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+  // validate department
+  if (department && department.trim().length < 5) {
+    return NextResponse.json(
+      {
+        error: "Department name must be at least 5 characters long",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+  // validate level
+  if (level && !["100", "200", "300", "400", "500"].includes(level.trim())) {
+    return NextResponse.json({
+      error: "Level must be one of 100, 200, 300, 400 or 500",
+    });
+  }
+  // validate school
+  if (school && school.trim().length < 5) {
+    return NextResponse.json(
+      {
+        error: "School name must be at least 5 characters long",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+  return NextResponse.json(
+    {
+      message: "User API is working with PUT method!",
+    },
+    {
+      status: 200,
+    },
+  );
 };
