5ed59325c5dedab77576fcf545162e4316b142d0
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-19 12:29:29 +0100

    feat: enhance class joining logic with detailed validation for student enrollment

--- app/api/join/route.js
+++ app/api/join/route.js
@@ -56,7 +56,11 @@ export const POST = async function POST(req) {
       );
     }
     // check if the student is already in the class
-    if (classExist?.students?.includes(studentId)) {
+    if (
+      classExist?.students?.find(
+        (id) => id?.toString() === studentId.toString(),
+      )
+    ) {
       return NextResponse.json(
         {
           error: "Student already in the class",
@@ -68,7 +72,7 @@ export const POST = async function POST(req) {
     }
     // check if the user meet the class rules
     const rules = classExist?.rules || { emailSuffix: "", departmentCode: [] };
-
+    // if email suffix rule exist, check if the user email ends with the suffix
     if (rules?.emailSuffix?.trim()) {
       const emailSuffix = rules.emailSuffix.trim().toLowerCase();
       if (!user.email.toLowerCase().endsWith(emailSuffix)) {
@@ -82,13 +86,73 @@ export const POST = async function POST(req) {
         );
       }
     }
-
+    // if department code rule exist, check if the user matric number contains any of the departmental codes
+    if (rules?.departmentCode?.length > 0) {
+      const userDepartment = rules?.departmentCode;
+      // check if the user matric number contains any of the departmental codes
+      const userHasDepartmentCode = userDepartment.find((code) =>
+        user?.matricNo?.toLowerCase().includes(code.toLowerCase()),
+      );
+      if (!userHasDepartmentCode) {
+        return NextResponse.json(
+          {
+            error: `Matric number must contain one of the following departmental codes: ${rules.departmentCode.join(", ")}`,
+          },
+          {
+            status: 400,
+          },
+        );
+      }
+    }
+    // check if user schhool matches class school
+    if (classExist?.school?.trim()) {
+      const classSchool = classExist?.school?.trim()?.toLowerCase();
+      const userSchool = user?.school?.trim()?.toLowerCase();
+      if (classSchool !== userSchool) {
+        return NextResponse.json(
+          {
+            error: `Student school does not match class school`,
+          },
+          {
+            status: 400,
+          },
+        );
+      }
+    }
+    // add student to class
+    const addStudent = await Classes.updateOne(
+      {
+        _id: classId,
+        students: { $ne: studentId },
+      },
+      {
+        $addToSet: { students: studentId },
+      },
+    );
+    console.log(addStudent);
+    if (addStudent.modifiedCount === 0) {
+      return NextResponse.json(
+        {
+          error: "Failed to join class. Please try again.",
+        },
+        {
+          status: 400,
+        },
+      );
+    }
     return NextResponse.json({
-      classId,
-      studentId,
-      rules,
+      message: "Joined class successfully",
+      classInfo: addStudent,
     });
   } catch (err) {
     console.log(err);
+    return NextResponse.json(
+      {
+        error: "An error occurred while joining the class",
+      },
+      {
+        status: 400,
+      },
+    );
   }
 };
