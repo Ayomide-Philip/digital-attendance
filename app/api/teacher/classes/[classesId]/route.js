939b066d02508b4a306e44604262f06041496cc7
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-21 00:34:14 +0100

    feat: implement authorization and parameter validation for class details retrieval

--- app/api/teacher/classes/[classesId]/route.js
+++ app/api/teacher/classes/[classesId]/route.js
@@ -1,9 +1,56 @@
 import { auth } from "@/auth";
+import { connectDatabase } from "@/lib/database/connectdb";
+import User from "@/lib/models/user.model";
 import { NextResponse } from "next/server";
 export const GET = auth(async function GET(req, { params }) {
+  if (!req?.auth || !req?.auth?.user) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized Access",
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+  const teacherId = req?.auth?.user?.id;
   const { classesId } = await params;
-  return NextResponse.json({
-    message: "Class details fetched successfully",
-    classesId,
-  });
+  if (!classesId || !classesId.trim() || !teacherId || !teacherId.trim()) {
+    return NextResponse.json(
+      {
+        error: "Invalid parameters",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+  try {
+    await connectDatabase();
+    const user = await User.findById(teacherId);
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "Unauthorized Access",
+        },
+        {
+          status: 401,
+        },
+      );
+    }
+    return NextResponse.json({
+      message: "Class details fetched successfully",
+      classesId,
+    });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      {
+        error: "An error occurred while fetching class details",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
 });
