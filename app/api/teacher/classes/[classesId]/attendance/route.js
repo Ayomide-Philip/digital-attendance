489ac77a276a143a5216f19a945460afdaff12dd
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-22 02:26:08 +0100

    feat: enhance attendance route validation and update schema to use classesId

--- app/api/teacher/classes/[classesId]/attendance/route.js
+++ app/api/teacher/classes/[classesId]/attendance/route.js
@@ -2,7 +2,42 @@ import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
 export const POST = auth(async function POST(req, { params }) {
+  // if (!req?.auth || !req?.auth?.user) {
+  //   return NextResponse.json(
+  //     { message: "Unauthorized Access" },
+  //     { status: 401 },
+  //   );
+  // }
+  // const userId = req?.auth?.user?.id;
+  const { classesId } = await params;
+  const {
+    userId,
+    title,
+    description,
+    location,
+    startTime,
+    endTime,
+    allowedRadius,
+  } = await req.json();
+  // validate input data here
+  if (!classesId || !classesId.trim() || !userId || !userId.trim()) {
+    return NextResponse.json({ error: "Invalid Parameters" }, { status: 400 });
+  }
+
+  if (!title || !title.trim()) {
+    return NextResponse.json({ error: "Title is required" }, { status: 400 });
+  }
+
+  if (title.trim().length < 5 || title.trim().length > 100) {
+    return NextResponse.json(
+      { error: "Title must be between 5 and 100 characters" },
+      { status: 400 },
+    );
+  }
+
   return NextResponse.json({
-    message: "POST attendance for class ",
+    message: "POST attendance for class",
+    userId,
+    classesId,
   });
 });
