d11295ab98f133ec7ffc9abf6d92ad062eb04592
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-05-01 10:58:56 +0100

    feat: implement attendance retrieval logic with user authentication and error handling

--- app/api/student/classes/[classesId]/attendance/[attendanceId]/route.js
+++ app/api/student/classes/[classesId]/attendance/[attendanceId]/route.js
@@ -1,4 +1,8 @@
 import { auth } from "@/auth";
+import { connectDatabase } from "@/lib/database/connectdb";
+import Classes from "@/lib/models/classes.model";
+import User from "@/lib/models/user.model";
+import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export const GET = auth(async function GET(req, { params }) {
@@ -15,9 +19,63 @@ export const GET = auth(async function GET(req, { params }) {
   const userId = req?.auth?.user?.id;
   const { classesId, attendanceId } = await params;
 
-  return NextResponse.json({
-    userId,
-    classesId,
-    attendanceId,
-  });
+  if (!userId.trim() || !classesId.trim() || !attendanceId.trim()) {
+    return NextResponse.json(
+      {
+        error: "Invalid Request Parameters",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  try {
+    await connectDatabase();
+    const user = await User.findById(new mongoose.Types.ObjectId(userId));
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "Invalid Parameters",
+        },
+        {
+          status: 401,
+        },
+      );
+    }
+
+    if (user?.role !== "student") {
+      return NextResponse.json(
+        {
+          error: "Forbidden Access",
+        },
+        {
+          status: 403,
+        },
+      );
+    }
+
+    const isUserEnrolled = await Classes.findOne({
+      _id: new mongoose.Types.ObjectId(classesId),
+      students: new mongoose.Types.ObjectId(userId),
+    });
+    return NextResponse.json(
+      {
+        isUserEnrolled: isUserEnrolled,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (err) {
+    return NextResponse.json(
+      {
+        error: "Failed to fetch attendance details. Please try again later.",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
 });
