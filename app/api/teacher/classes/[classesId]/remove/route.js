ef3c338a94b9eb8f161ad4fda5d06e0467e36b11
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-05-01 22:24:33 +0100

    feat: enhance student removal route with user authentication and error handling

--- app/api/teacher/classes/[classesId]/remove/route.js
+++ app/api/teacher/classes/[classesId]/remove/route.js
@@ -1,15 +1,44 @@
+import { connectDatabase } from "@/lib/database/connectdb";
+import User from "@/lib/models/user.model";
+import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export async function PUT(req, { params }) {
   const searchParams = await req.nextUrl.searchParams;
   const studentId = searchParams.get("studentId");
-  const { classesId, userId } = await params;
+  const { classesId } = await params;
+  const { userId } = await req.json();
 
   if (!studentId?.trim() || !classesId?.trim() || !userId?.trim()) {
     return NextResponse.json({ error: "Invalid parameters" }, { status: 400 });
   }
 
   try {
+    await connectDatabase();
+    const user = await User.findById(new mongoose.Types.ObjectId(userId));
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "Unauthorized Access",
+        },
+        {
+          status: 401,
+        },
+      );
+    }
+
+    if (user.role !== "teacher") {
+      return NextResponse.json(
+        {
+          error: "Forbidden Access",
+        },
+        {
+          status: 403,
+        },
+      );
+    }
+
     return NextResponse.json({
       studentId,
       classesId,
