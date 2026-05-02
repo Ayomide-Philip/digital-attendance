fde1cfc4bb9590edf079582dec1440c793808a72
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-05-02 01:51:49 +0100

    feat: implement GET endpoint for class stats with authorization and parameter validation

--- app/api/teacher/classes/[classesId]/stats/route.js
+++ app/api/teacher/classes/[classesId]/stats/route.js
@@ -1,1 +1,30 @@
-export const GET =aut( async function GET() {});
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+export const GET = auth(async function GET(req, { params }) {
+  if (!req?.auth || !req?.auth?.user) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized Access",
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+  const userId = req.auth.user.id;
+  const { classesId } = await params;
+
+  if (!classesId?.trim() || !userId.trim()) {
+    return NextResponse.json(
+      {
+        error: "Invalid Parameters",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  try{}catch(err){}
+});
