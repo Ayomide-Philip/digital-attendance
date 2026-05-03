9aa921978c7d24ec2f4c1ee674a632d7923ddce1
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-05-03 23:28:14 +0100

    feat: implement GET endpoint for teachers to retrieve all students with authorization checks

--- app/api/teacher/students/route.js
+++ app/api/teacher/students/route.js
@@ -1,5 +1,9 @@
 import { auth } from "@/auth";
+import { connectDatabase } from "@/lib/database/connectdb";
+import User from "@/lib/models/user.model";
 import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+import Classes from "@/lib/models/classes.model";
 
 export const GET = auth(async function GET(req) {
   if (!req?.auth || !req?.auth?.user) {
@@ -13,12 +17,68 @@ export const GET = auth(async function GET(req) {
     );
   }
   const userId = req?.auth?.user?.id;
-  return NextResponse.json(
-    {
-      message: "Get all teacher students",
-    },
-    {
-      status: 200,
-    },
-  );
+  try {
+    await connectDatabase();
+    const user = await User.findById(new mongoose.Types.ObjectId(userId));
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "Unauthorized Access",
+        },
+        {
+          status: 401,
+        },
+      );
+    }
+
+    if (user?.role !== "teacher") {
+      return NextResponse.json(
+        {
+          error: "Forbidden Access",
+        },
+        {
+          status: 403,
+        },
+      );
+    }
+
+    const classes = await Classes.find({
+      teacher: new mongoose.Types.ObjectId(userId),
+    })
+      .populate("students", "name email department")
+      .select("name students")
+      .lean();
+
+    const studentBelongToClasses = classes?.map((c) => {
+      return c.students?.map((s) => {
+        return {
+          ...s,
+          className: c.name,
+          classId: c._id,
+        };
+      });
+    });
+
+    return NextResponse.json(
+      {
+        message: "Get all teacher students",
+        // classes: classes,
+        students: studentBelongToClasses,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      {
+        error: "Unable to get all students",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
 });
