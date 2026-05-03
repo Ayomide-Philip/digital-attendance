f75f10d89c5251bc41a7134ab1a7137a834210f8
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-05-04 00:02:23 +0100

    refactor: remove unused buttons from StudentsTab component

--- app/(dashboard)/dashboard/teachers/components/studentsTab.jsx
+++ app/(dashboard)/dashboard/teachers/components/studentsTab.jsx
@@ -5,17 +5,6 @@ import StudentList from "./StudentList";
 export default function StudentsTab({ students, classId }) {
   return (
     <div className="space-y-4">
-      <div className="flex flex-wrap gap-2">
-        <Button className="h-10 rounded-xl px-4">
-          <Plus className="size-4" />
-          Add Student
-        </Button>
-        <Button variant="outline" className="h-10 rounded-xl px-4">
-          <Upload className="size-4" />
-          Import Students
-        </Button>
-      </div>
-
       <StudentList students={students} classId={classId} />
     </div>
   );
