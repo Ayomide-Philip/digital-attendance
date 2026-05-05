d7de98260e57ffa439c36b59f59ebe0bb5c6bb0f
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-05-05 22:02:44 +0100

    feat: add authorization checks for teacher stats endpoint

--- app/api/teacher/stats/route.js
+++ app/api/teacher/stats/route.js
@@ -2,7 +2,42 @@ import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
 export const GET = auth(async function GET(req) {
-  return NextResponse.json({
-    message: "Hello, teacher! This is your stats endpoint.",
-  });
+  if (!req?.auth || !req?.auth?.user) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized Access",
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+
+  const userId = req?.auth?.user?.id;
+  if (!userId) {
+    return NextResponse.json(
+      {
+        error: "Invalid request parameters",
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+
+  try {
+    return NextResponse.json({
+      message: "Hello, teacher! This is your stats endpoint.",
+    });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      {
+        error: "Unable to fetch stats data",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
 });
