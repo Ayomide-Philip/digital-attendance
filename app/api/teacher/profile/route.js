8dc7a40d1d092145e7dc7bba57773bff3c0229a2
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-05-18 00:56:49 +0100

    updating my codebase

--- app/api/teacher/profile/route.js
+++ app/api/teacher/profile/route.js
@@ -1,4 +1,6 @@
 import { auth } from "@/auth";
+import { connectDatabase } from "@/lib/database/connectdb";
+import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export const GET = auth(async function GET(req) {
@@ -9,6 +11,34 @@ export const GET = auth(async function GET(req) {
       },
       {
         status: 401,
+      },
+    );
+  }
+  const userId = req?.auth?.user?.id;
+  if (!mongoose.Types.ObjectId.isValid(userId?.trim())) {
+    return NextResponse.json(
+      {
+        error: "Invalid Parameters",
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+
+  try {
+    await connectDatabase();
+    return NextResponse.json({
+      message: "GET user profile sucessfully",
+    });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      {
+        error: "Unable to fetch teacher profile",
+      },
+      {
+        status: 500,
       },
     );
   }
