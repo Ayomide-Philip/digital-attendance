0e5ef7535c04580af1eb74b0e9e048ca3fa8fc18
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-05-23 19:29:14 +0100

    feat: implement GET route for student profile with authorization check

---
+++ app/api/student/profile/route.js
@@ -0,0 +1,33 @@
+import { auth } from "@/auth";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export const GET = auth(async function GET(req) {
+  if (!req?.auth || !req?.auth?.user) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized Access",
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+
+  const userId = req?.auth?.user?.id;
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized Access",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  return NextResponse.json({
+    message: "Profile data",
+  });
+});
